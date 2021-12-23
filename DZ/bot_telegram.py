277607cad9270b5222130fb2688e5ed14ea3b2f3
Angelina Blizneva <71411417+Draconic1@users.noreply.github.com>
2021-12-23 14:13:46 +0300

    Add files via upload

--- DZ/bot_telegram.py
+++ DZ/bot_telegram.py
@@ -3,10 +3,11 @@ from create_bot import dp
 from handlers import client, admin
 from data_base import sqlite_db
 
+connectionString = 'tour.db'
 
 async def on_startup(_):
     print('Бот в сети')
-    sqlite_db.sql_start()
+    sqlite_db.sql_start(connectionString)
 
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
